c5deb589bed61c3d17148b8c1f42891f23660de4
Asnoyon <blackpain1100@gmail.com>
2023-09-15 07:06:13 +0600

    bootstrap submit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,69 +2,69 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div class="container">
-    <div class="myCard">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="myLeftCtn">
-            <div class="myForm text-center ">
+    
+    <div className="myCard">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="myLeftCtn">
+            <div className="myForm text-center ">
               <header>Create new account</header>
-              <div class="form-group pb-3">
-                <i class="fa-solid fa-user"></i>
+              <div className="form-group pb-3">
+                <i className="fa-solid fa-user"></i>
                 <input
                   type="text"
-                  class="myInput"
+                  className="myInput"
                   placeholder="Username"
                   id="username"
                   required
                 />
               </div>
 
-              <div class="form-group pb-3">
-                <i class="fa-solid fa-envelope"></i>
+              <div className="form-group pb-3">
+                <i className="fa-solid fa-envelope"></i>
                 <input
                   type="email"
-                  class="myInput"
+                  className="myInput"
                   placeholder="Email"
                   id="email"
                   required
                 />
               </div>
 
-              <div class="form-group pb-3">
-                <i class="fa-solid fa-lock"></i>
+              <div className="form-group pb-3">
+                <i className="fa-solid fa-lock"></i>
                 <input
                   type="password"
-                  class="myInput"
+                  className="myInput"
                   placeholder="Password"
                   id="password"
                   required
                 />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 
                   <input type="checkbox" id="check_1" name="check_1" required /> <small>I read and agree to Terms & Conditions</small>
-                  <div class="invalid-feedback">You must check the box</div>
+                  <div className="invalid-feedback">You must check the box</div>
                 
               </div>
 
-              <input type="submit" value="Create Account" class="butt"/>
+              <input type="submit" value="Create Account" className="butt"/>
             </div>
           </div>
         </div>
 
-        <div class="col-md-6">
-          <div class="myRightCtn">
-            <div class="box">
+        <div className="col-md-6">
+          <div className="myRightCtn">
+            <div className="box">
               <header>Hello World</header>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate incidunt odit voluptatibus dignissimos totam voluptas maxime nam eum veritatis culpa!</p>
-              <input type="button" value="Learn More" class="butt_out"/>
+              <input type="button" value="Learn More" className="butt_out"/>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  
   )
 }
 
